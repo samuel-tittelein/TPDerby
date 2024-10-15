8a513255a5c4150ac464c2e4b6b77be181097426
samuel_tittelein <samuel_tittelein@ens.univ-artois.fr>
2024-10-15 15:30:53 +0200

    :bug: bugfix

--- src/CreationBD.java
+++ src/CreationBD.java
@@ -9,27 +9,25 @@ public class CreationBD {
                 conn.close();
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 
 
     // Méthode statique pour créer une base de données
     public static void creerBD(String nomBD) {
-        try (Connection conn = DriverManager.getConnection("jdbc:derby:" + nomBD + ";create=true")) {
-            System.out.println("Base de données " + nomBD + " créée.");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        Connection conn = seConnecter(nomBD);
+        System.out.println("Base de données " + nomBD + " créée.");
+        fermerConnexion(conn);
     }
 
     // Méthode statique pour se connecter à une base de données
     public static Connection seConnecter(String nomBD) {
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection("jdbc:derby:" + nomBD);
+            conn = DriverManager.getConnection("jdbc:derby:db/" + nomBD + ";create=true");
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
         return conn;
     }
@@ -46,72 +44,68 @@ public class CreationBD {
                 System.out.println("La table " + nomTable + " existe déjà.");
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 
 
     public static void dropTables(String nomBD) {
-        try (Connection conn = seConnecter(nomBD); Statement stmt = conn.createStatement()) {
-            stmt.execute("DROP TABLE Pret");
-            stmt.execute("DROP TABLE Article");
-            stmt.execute("DROP TABLE Proprietaire");
-            stmt.execute("DROP TABLE Client");
+        try (Connection conn = seConnecter(nomBD); Statement stmt = conn.createStatement()){
+            stmt.executeUpdate("DROP TABLE Pret");
+            stmt.executeUpdate("DROP TABLE Article");
+            stmt.executeUpdate("DROP TABLE Proprietaire");
+            stmt.executeUpdate("DROP TABLE Client");
 
             System.out.println("Tables supprimées.");
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 
 
     public static void main(String[] args) {
-
-        String nomBD = "db/BASEDEBZ";
-        String urlBD = "jdbc:derby:db/" + nomBD + ";create=true";
-        Connection connexion = null;
-        try {
-            connexion = DriverManager.getConnection(urlBD);
-
-            creerBD(nomBD);
-            dropTables(nomBD);
-
-            String client = "Client";
-            String creationTableClient = "CREATE TABLE " + client + " (" +
-                                         "id INT PRIMARY KEY, " +
-                                         "nom VARCHAR(50), " +
-                                         "prenom VARCHAR(50), " +
-                                         "age INT)";
-            creerTable(nomBD, creationTableClient, client);
-
-            String proprietaire = "Proprietaire";
-            String creationTableProprietaire = "CREATE TABLE " + proprietaire +" (" +
-                                               "id INT PRIMARY KEY, " +
-                                               "nom VARCHAR(50), " +
-                                               "prenom VARCHAR(50))";
-            creerTable(nomBD, creationTableProprietaire, proprietaire);
-
-            String article = "Article";
-            String creationTableArticle = "CREATE TABLE " + article + " (" +
-                                          "id INT PRIMARY KEY, " +
-                                          "nomA VARCHAR(50), " +
-                                          "cout DECIMAL(10, 2))";
-            creerTable(nomBD, creationTableArticle, article);
-
-            String pret = "Pret";
-            String creationTablePret = "CREATE TABLE " + pret + " (" +
-                                       "idClient INT, " +
-                                       "idArticle INT, " +
-                                       "nb_jours INT, " +
-                                       "FOREIGN KEY (idClient) REFERENCES Client(id), " +
-                                       "FOREIGN KEY (idArticle) REFERENCES Article(id))";
-            creerTable(nomBD, creationTablePret, pret);
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            fermerConnexion(connexion);
+        String nomBD;
+        if (args.length != 0) {
+            nomBD = args[0];
+        } else {
+            nomBD = "bd_prets";
         }
+        creerBD(nomBD);
+        dropTables(nomBD);
+
+        String client = "Client";
+        String creationTableClient = "CREATE TABLE " + client + " (" +
+                                     "id INT PRIMARY KEY, " +
+                                     "nom VARCHAR(50), " +
+                                     "prenom VARCHAR(50), " +
+                                     "age INT)";
+        creerTable(nomBD, creationTableClient, client);
+
+        String proprietaire = "Proprietaire";
+        String creationTableProprietaire =
+                "CREATE TABLE " + proprietaire + " (" +
+                "id INT PRIMARY KEY, " +
+                "nom VARCHAR(50), " +
+                "prenom VARCHAR(50))";
+        creerTable(nomBD, creationTableProprietaire, proprietaire);
+
+        String article = "Article";
+        String creationTableArticle = "CREATE TABLE " + article + " (" +
+                                      "id INT PRIMARY KEY, " +
+                                      "nomA VARCHAR(50), " +
+                                      "cout DECIMAL(10, 2))";
+        creerTable(nomBD, creationTableArticle, article);
+
+        String pret = "Pret";
+        String creationTablePret = "CREATE TABLE " + pret + " (" +
+                                   "idClient INT, " +
+                                   "idArticle INT, " +
+                                   "nb_jours INT, " +
+                                   "FOREIGN KEY (idClient) REFERENCES Client(id), "
+                                   +
+                                   "FOREIGN KEY (idArticle) REFERENCES Article(id))";
+        creerTable(nomBD, creationTablePret, pret);
+
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        CreationBD.creerBD("db/BASEDEBZ");
+        CreationBD.main(new String[] { "bd_prets"});
     }
 
 }
